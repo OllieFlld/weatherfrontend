dc719de2c54102ebb145f818d1e4ab8a847a7e45
ollie <oliverfield99@gmail.com>
2025-11-20 21:56:18 +0000

    another test

--- script.py
+++ script.py
@@ -5,15 +5,15 @@ import requests
 from io import BytesIO
 from os import environ
 
-from inky.auto import auto
+import inky
 
-display = auto()
+display = inky.Inky_Impressions_7
 
 backendurl = os.environ['BACKEND_URL']
 
 result = requests.get(backendurl + '/refresh')
 
 img = Image.open(BytesIO(result.content))
-display.set_image(img)
+display.set_image(img, saturation=0.5)
 
 display.show()
